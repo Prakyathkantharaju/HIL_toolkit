352a2ced5136add4335b41f7b3b92ba27020d438
Prakyath Kantharaju <prakyathk33@gmail.com>
2022-12-10 06:43:07 -0600

    offline estimation and docmentation

--- scripts/metabolic_estimation.py
+++ scripts/metabolic_estimation.py
@@ -14,27 +14,6 @@ from HIL.cost_processing.metabolic_cost.ppe import PPE
 # HIL plotting utils
 from HIL.plotting.utils import plot_elipses, plot_mean_estimation
 
-# plotting ellipses
-# from gmr import plot_error_ellipse
-
-# def plot_elipses(means, covariances, ax):
-#     color_iter = itertools.cycle(["navy", "c", "cornflowerblue", "gold", "darkorange"])
-#     for i, (mean, covar, color) in enumerate(zip(means, covariances, color_iter)):
-#         v, w = linalg.eigh(covar) #type:ignore
-#         v = 2.0 * np.sqrt(2.0) * np.sqrt(v)
-#         u = w[0] / linalg.norm(w[0]) #type:ignore
-
-#         angle = np.arctan(u[1] / u[0])
-#         angle = 180.0 * angle / np.pi  # convert to degrees
-#         ell = mpl.patches.Ellipse(mean, v[0], v[1], angle=180.0 + angle, color=color) #type:ignore
-#         ell.set_clip_box(ax.bbox)
-#         ell.set_alpha(0.1)
-#         ax.add_artist(ell)
-
-# def plot_mean_estimation(mean, std, ax):
-#     ax.plot(mean[0], 0, "o", color="red", markersize=5)
-#     ax.plot([mean[0] + 3 *std[0][0], mean[0] - 3*std[0][0]],[0,0], color="b", linewidth=5, alpha=0.5)
-
 
 
 
@@ -48,45 +27,67 @@ steady_state_estimation_mean = []
 steady_state_estimation_std = []
 steady_state_time = []
 ax_list = []
-
+fig, ax = plt.subplots(2,1, figsize=(20, 10))
 # estimating the metabolic cost
-for i in tqdm(range(10, met_data.shape[0])):
+for i in tqdm(range(15, met_data.shape[0])):
     met = met_data[:i, 0]
     time = met_data[:i, 1]
 
+
+    # Estimating the metabolic cost
+    # you can change the upsampling parameter to get a better estimation
     ppe = PPE()
     mean, std = ppe.estimate(met, time)
     # print(mean, std)
+
+    # Done with the estimation not plotting the results
+    # Need to find a better way to do this, but for now this is fine.
     steady_state_estimation_mean.append(mean)
     steady_state_estimation_std.append(std)
     steady_state_time.append(time[-1])
-    fig, ax = plt.subplots()
-    ax.plot(ppe.phase_plane[:, 0], ppe.phase_plane[:, 1], label="phase plane") #type:ignore
-    plot_elipses(ppe.gmm.means_, ppe.gmm.covariances_, ax) #type:ignore
-    plot_mean_estimation(mean, std, ax)
-    plt.xlabel("Metabolic cost (W)")
-    plt.ylabel("d(Metabolic cost) (W)")
+
+    # comment out plotting if you don't want to plot the results or speed up the estimation process.
+    # Phase plane plotting.
+    ax[0].plot(ppe.phase_plane[:, 0], ppe.phase_plane[:, 1], label="phase plane") #type:ignore
+    plot_elipses(ppe.gmm.means_, ppe.gmm.covariances_, ax[0]) #type:ignore
+    plot_mean_estimation(mean, std, ax[0], label = "estimated mean")
+    ax[0].set_xlabel("Metabolic cost (W)")
+    ax[0].set_ylabel("d(Metabolic cost) (W)")
+
+    # Plotting the metabolic cost
+    ax[1].plot(time, met, label="metabolic cost")
+    ax[1].plot(steady_state_time, steady_state_estimation_mean, label="estimated mean")
+    ax[1].set_ylabel("Metabolic cost (W)")
+    ax[1].set_xlabel("Time (s)")
+
+    # legend
+    ax[0].legend()
+    ax[1].legend()
+
+    # This is to record the video
     canvas = FigureCanvas(fig)
     canvas.draw()       # draw the canvas, cache the renderer
-
     image = np.frombuffer(canvas.tostring_rgb(), dtype='uint8') #type:ignore
     image = image.reshape(fig.canvas.get_width_height()[::-1] + (3,))
-
     ax_list.append(image)
 
+    # not clearing the last plot
+    if i == met_data.shape[0] - 1:
+        break
+    ax[0].cla()
+    ax[1].cla()
+
+plt.savefig("Results/figures/metabolic_cost_estimation.png")
 
 plt.close("all")
-# # plotting the results
-# plt.figure()
-# plt.plot(met_data[:, 1], met_data[:,0], label="raw metabolic data", alpha = 0.5)
-# plt.plot(steady_state_time, steady_state_estimation_mean, label="steady state mean")
-# plt.show()
-writer = imageio.get_writer('test.mp4', fps=20)
+writer = imageio.get_writer('Results/videos/metabolic_cost_estimation.mp4', fps=20)
 
 for im in ax_list:
     writer.append_data(im)
 writer.close()
 
+# saving it as gif
+imageio.mimsave('Results/videos/metabolic_cost_estimation.gif', ax_list, fps=20)
 
 
 
